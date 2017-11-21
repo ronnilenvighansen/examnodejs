a8b98f68e5a8212b931eefcba2e93683211e1a29
ronnilenvighansen <ronnilenvighansen@gmail.com>
2017-11-21 23:04:25 +0100

    Fourth commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
+var bodyParser = require('body-parser');
 var path = __dirname + '/wwwroot/html';
 
 app.use(bodyParser.urlencoded({extended : true}));
