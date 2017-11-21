53755ae6aa4b8876496f1ae282f4fb9471063ad9
ronnilenvighansen <ronnilenvighansen@gmail.com>
2017-11-21 22:55:17 +0100

    Third commit

--- index.js
+++ index.js
@@ -40,4 +40,4 @@ app.get('/about', function(req, res)
 app.listen(process.env.PORT || 3000, function()
 {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-})
+});
